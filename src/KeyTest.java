6e0bb4531d988b0abfc86aaa66ec53eefd093713
Daniel Carvalho <danielscarvalho@gmail.com>
2019-03-31 13:38:16 -0300

    First commmit...

---
+++ src/KeyTest.java
@@ -0,0 +1,16 @@
+
+public class KeyTest {
+	
+	private String key = "d849c1cf35244938ae82e521b92b720ab48b8e9ed092d5de92c2464ef5244b9b";
+    private String user = "Scott";
+    private String password = "Tiger";
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print(new java.util.Date());
+		System.out.println(" -  Key test...");
+
+	}
+
+}
